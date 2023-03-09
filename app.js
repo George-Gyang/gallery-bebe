fde419f2b48e2788c2f24d7024f2455da6d3408f
George-Gyang <georgegyang3@gmail>
2023-03-09 15:57:40 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,121 @@
+// alert("I'm connected!!")
+// $(document).ready(function(){
+//     $(".owl-carousel").owlCarousel();
+//   });
+// $('.owl-carousel').owlCarousel({
+//     loop: true,
+//     margin: 10,
+//     nav: true,
+//     responsive: {
+//         0: {
+//             items: 1
+//         },
+//         600: {
+//             items: 3
+//         },
+//         1000: {
+//             items: 5
+//         }
+//     }
+// })
+
+var slider = tns({
+    container: '.my-slider',
+    items: 2,
+    edgePadding: 10,
+    gutter: 20,
+
+    slideBy: 'page',
+    autoplay: true,
+    navPosition: "bottom",
+    controls: false,
+    responsive: {
+        700: {
+            edgePadding: 10,
+            gutter: 15,
+            items: 4
+        }
+    }
+});
+
+var sliderTwo = tns({
+    container: '.my-slider-2',
+    items: 2,
+    // edgePadding: 10,
+    gutter: 40,
+
+    slideBy: 'page',
+    autoplay: true,
+    navPosition: "bottom",
+    controls: false,
+    responsive: {
+        700: {
+            // edgePadding: 10,
+            gutter: 30,
+            items: 6
+        }
+    }
+});
+
+// var sliderTwo = tns({
+//     container: '.my-slider-1',
+//     items: 2,
+//     edgePadding: 10,
+//     gutter: 20,
+//     autoplayTimeout:4000,
+//     slideBy: 'page',
+//     autoplay: true,
+//     navPosition: "bottom",
+//     controls: false,
+//     responsive: {
+//         700: {
+//             edgePadding: 20,
+//             gutter: 20,
+//             items: 6
+//         }
+//     }
+// });
+
+var myLogo = tns({
+    container: '.my-logo',
+    items: 2,
+    edgePadding: 10,
+    gutter: 20,
+    autoplayTimeout:4000,
+    slideBy: 2,
+    autoplay: true,
+    autoplayPosition: "autoplay",
+    controlsPosition: "bottom",
+    navPosition: "bottom",
+    controls: false,
+    autoplayText:[ "▶","❚❚"],
+    responsive: {
+        700: {
+            edgePadding: 20,
+            gutter: 20,
+            items: 6
+        }
+    }
+});
+
+// ALERT JS
+const alertPlaceholder = document.getElementById('liveAlertPlaceholder')
+
+const alert = (message, type) => {
+  const wrapper = document.createElement('div')
+  wrapper.innerHTML = [
+    `<div class="alert alert-${type} alert-dismissible" role="alert">`,
+    `   <div>${message}</div>`,
+    '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+    '</div>'
+  ].join('')
+
+  alertPlaceholder.append(wrapper)
+}
+
+const alertTrigger = document.getElementById('liveAlertBtn')
+if (alertTrigger) {
+  alertTrigger.addEventListener('click', () => {
+    alert('Nice, you triggered this alert message!', 'success')
+  })
+}
